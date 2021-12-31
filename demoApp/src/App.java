137d8897e6ac60c72df38e77b6077f74bc157cbc
aasusudarsan <31433229+aasusudarsan@users.noreply.github.com>
2021-12-31 21:41:32 +0530

    delete demo app

--- demoApp/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("this is my second line of code");
-        System.out.println("this is my third line of code");
-    }
-}
